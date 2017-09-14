e064228d63647d3d3ddd2897296798290a2cea56
Lily <lilyliu@cyagen.com>
2017-09-14 10:39:45 +0800

    Upload new file

---
+++ PROT.py
@@ -0,0 +1,12 @@
+str="AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+dict={'UUU':'F','CUU':'L','AUU':'I','GUU':'V','UUC':'F','CUC':'L','AUC':'I','GUC':'V','UUA':'L','CUA':'L','AUA':'I','GUA':'V','UUG':'L','CUG':'L','AUG':'M','GUG':'V','UCU':'S','CCU':'P','ACU':'T','GCU':'A','UCC':'S',      'CCC':'P',     'ACC':'T',      'GCC':'A','UCA':'S',     'CCA':'P',     'ACA':'T',    'GCA' :'A','UCG':'S',      'CCG':'P','ACG':'T','GCG':'A','UAU':'Y','CAU':'H', 'AAU':'N','GAU':'D','UAC':'Y','CAC':'H',      'AAC':'N',      'GAC':'D','UAA':'Stop',   'CAA':'Q',      'AAA':'K',      'GAA':'E','UAG':'Stop',   'CAG':'Q','AAG':'K','GAG':'E','UGU':'C',      'CGU':'R',      'AGU':'S',    'GGU':'G','UGC':'C', 'CGC':'R',      'AGC':'S',  'GGC':'G','UGA':'Stop', 'CGA':'R', 'AGA':'R', 'GGA':'G','UGG':'W','CGG':'R', 'AGG':'R','GGG':'G',}
+def change(x):
+    i=0
+    while i <len(x):
+        if dict[x[i:i+3]]=='Stop':
+            break
+        else:
+            print dict[x[i:i+3]],
+        i+=3
+    return
+change(str)
