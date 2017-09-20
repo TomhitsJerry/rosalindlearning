f864edf8516998099a8d297db85680a2bd85f64a
Lily <lilyliu@cyagen.com>
2017-09-20 13:42:43 +0800

    Upload new file

---
+++ IEV.py
@@ -0,0 +1,9 @@
+print "input couples number:"
+i=raw_input("AA-AA:")
+j=raw_input("AA-Aa:")
+k=raw_input("AA-aa:")
+l=raw_input("Aa-Aa:")
+m=raw_input("Aa-aa:")
+n=raw_input("aa-aa:")
+i,j,k,l,m,n=int(i),int(j),int(k),int(l),int(m),int(n)
+print 2.0*(i+j+k)+1.5*l+m
