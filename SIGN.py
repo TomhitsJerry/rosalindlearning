31018d24a97c1dcb1e1fad7f66fd6b471b6a7f72
Lily <lilyliu@cyagen.com>
2017-09-27 16:53:51 +0800

    Upload new file

---
+++ SIGN.py
@@ -0,0 +1,16 @@
+import itertools
+n = 6
+permutation = []
+nr = 0
+for i in itertools.permutations(list(range(1, n + 1))):
+    for j in itertools.product([-1, 1], repeat=len(list(range(1, n + 1)))):
+        perm = [a * sign for a, sign in zip(i, j)]
+        permutation.append(perm)
+        nr += 1
+
+print(nr)
+for i in permutation:
+    for j in i:
+        print j,
+    print ""
+
